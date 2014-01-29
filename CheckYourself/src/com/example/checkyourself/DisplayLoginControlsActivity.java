47d755e5aa1517b69240dcc8a750e938104b066c
Prachi Fulay <pfulay3@gatech.edu>
2014-01-29 12:54:43 -0500

    Removed text field from the welcome screen. Changed the Password to input type "textPassword."

--- CheckYourself/src/com/example/checkyourself/DisplayLoginControlsActivity.java
+++ CheckYourself/src/com/example/checkyourself/DisplayLoginControlsActivity.java
@@ -30,7 +30,7 @@ public class DisplayLoginControlsActivity extends Activity {
 	/**
 	 * Set up the {@link android.app.ActionBar}, if the API is available.
 	 */
-	@TargetApi(Build.VERSION_CODES.HONEYCOMB)
+	@TargetApi(Build.VERSION_CODES.HONEYCOMB) 
 	private void setupActionBar() {
 		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
 			getActionBar().setDisplayHomeAsUpEnabled(true);
--- CheckYourself/src/com/example/checkyourself/MainActivity.java
+++ CheckYourself/src/com/example/checkyourself/MainActivity.java
@@ -9,7 +9,7 @@ import android.widget.EditText;
 
 public class MainActivity extends Activity {
 
-	@Override
+	@Override 
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
