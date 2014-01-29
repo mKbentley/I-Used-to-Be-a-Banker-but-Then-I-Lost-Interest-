b21bb3280b504868bcfb520578bfb3fae8597e21
Prachi Fulay <pfulay3@gatech.edu>
2014-01-29 12:26:01 -0500

    Clicking the Login button takes you to another screen, which displays text fields for User ID and Password.

--- CheckYourself/gen/com/example/checkyourself/R.java
+++ CheckYourself/gen/com/example/checkyourself/R.java
@@ -24,14 +24,18 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080001;
-        public static final int edit_message=0x7f080000;
+        public static final int action_settings=0x7f080003;
+        public static final int edit_message=0x7f080002;
+        public static final int edit_password=0x7f080001;
+        public static final int edit_userID=0x7f080000;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
+        public static final int activity_display_login_controls=0x7f030000;
+        public static final int activity_main=0x7f030001;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int display_login_controls=0x7f070000;
+        public static final int main=0x7f070001;
     }
     public static final class string {
         public static final int action_settings=0x7f050004;
@@ -39,7 +43,13 @@ public final class R {
         public static final int button_login=0x7f050002;
         public static final int button_register=0x7f050003;
         public static final int edit_message=0x7f050001;
+        public static final int edit_password=0x7f05000b;
+        public static final int edit_userID=0x7f05000a;
+        public static final int hello_world=0x7f050007;
+        public static final int password=0x7f050009;
+        public static final int title_activity_display_login_controls=0x7f050006;
         public static final int title_activity_main=0x7f050005;
+        public static final int user_id=0x7f050008;
     }
     public static final class style {
         /** 
